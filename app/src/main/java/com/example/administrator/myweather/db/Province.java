dcc4d64b0ca0ad01a26e902df6af5cb42c1c04f3
wu <gem.wu@qq.com>
2017-09-14 11:52:30 +0800

    加入创建数据库和表的各项配置

---
+++ app/src/main/java/com/example/administrator/myweather/db/Province.java
@@ -0,0 +1,37 @@
+package com.example.administrator.myweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by Administrator on 2017/9/14 0014.
+ */
+
+public class Province extends DataSupport {
+    private int id;
+    private String provinceName;
+    private int provinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+}
